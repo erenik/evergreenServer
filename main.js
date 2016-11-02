953c99149b5008364af492841cde7b78b0906aac
Axel Forsberg <Axel Forsberg>
2016-11-02 11:34:05 +0100

    Added a page

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 console.log("Hello world!");
 
+// Libraries
 var http = require('http');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+   res.send('Hello World');
+})
+
+app.get('/test', function(req, res) {
+   res.send('Hello test');
+})
+
+var port = 8800;
+
+var server = app.listen(port, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+/**
 var server = http.createServer(function (request, response) {
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.end("Hello world\nVery awesome");
 });
-
-var port = 8800;
 server.listen(port);
 
+*/
+
+
 console.log("Server running at http://127.0.0.1:"+port);
+
+console.log("log moar");
 
